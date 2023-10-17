be43c561c52ac2de6f75ff8230d670ccfc8151b5
W. Pandula <52339029+PanduDcau@users.noreply.github.com>
2023-10-17 21:36:08 +0530

    Live Audio Recognition

---
+++ Speech To Text/Live Recog.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+import sys
+
+duration = int(input("Duration of Microphone is Open :"));
+
+# initialize the recognizer
+r = sr.Recognizer()
+print("Please talk")
+
+with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    audio_data = r.record(source, duration=duration)
+    print("Recognizing...")
+    # convert speech to text
+    text = r.recognize_google(audio_data)
+    print(text)
