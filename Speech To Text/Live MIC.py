a143eb205646719dbc8b815cf316ef1090e98abe
W. Pandula <52339029+PanduDcau@users.noreply.github.com>
2023-10-28 23:47:47 +0530

    Live Mic File

---
+++ Speech To Text/Live MIC.py
@@ -0,0 +1,20 @@
+import speech_recognition as sr
+import sys
+# pip install -U spacy
+# python -m spacy download en_core_web_sm
+import spacy
+import re
+
+duration = int(input("Duration of Microphone is Open :"));
+
+# initialize the recognizer
+r = sr.Recognizer()
+print("Please talk")
+
+with sr.Microphone() as source:
+    # read the audio data from the default microphone
+    audio_data = r.record(source, duration=duration)
+    print("Recognizing...")
+    # convert speech to text
+    text = r.recognize_google(audio_data)
+    print(text)
